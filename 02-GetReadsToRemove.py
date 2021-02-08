f0bc9292de511a641b9ee4f24ac596541850ec03
Vinícius Henrique Franceschini dos Santos <vinicius6.santos@usp.br>
2021-02-08 17:17:24 -0300

    Create 02-GetReadsToRemove.py

---
+++ 02-GetReadsToRemove.py
@@ -0,0 +1,36 @@
+import matplotlib; matplotlib.use('Agg')
+from matplotlib_venn import venn3, venn3_circles
+from matplotlib import pyplot as plt
+import argparse
+
+
+def GetReadsToRemove(bc):
+        with open('barcode0'+bc+'.fixedMGEs.txt','r') as fxd:
+                fixed = []
+                for line in fxd.readlines():
+                        fixed.append(line)
+        with open('barcode0'+bc+'.optionalMGEs.txt','r') as opt:
+                optional = []
+                for line in opt.readlines():
+                        optional.append(line)
+        with open('barcode0'+bc+'.all-reads.txt', 'r') as allids:
+                allreads = []
+                for line in allids.readlines():
+                        allreads.append(line)
+        venn3([set(allreads), 
+                   set(fixed),
+                   set(optional)],
+                   set_labels=('total reads', 'fixed', 'optional'))
+        plt.savefig('bc0'+bc+'.svg')
+        print('Writing txt file')
+        ToRemove = set(optional).difference(set(fixed))
+        with open('barcode0'+bc+'.to-remove.txt','w') as file:
+                for read in ToRemove:
+                        file.write(read)
+
+parser = argparse.ArgumentParser(description='Plot Venn diagram of barcode data')
+parser.add_argument('--barcode',
+                    help='barcode identifier')
+
+args = parser.parse_args()
+GetReadsToRemove(args.barcode)
